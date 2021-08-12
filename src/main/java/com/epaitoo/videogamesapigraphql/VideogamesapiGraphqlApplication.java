88e6394a325fb588ee6e3e6f1a862c12ab12fd8b
Eugene Paitoo <eugenepaitoo@gmail.com>
2021-08-12 23:32:58 +0000

    initial commit

---
+++ src/main/java/com/epaitoo/videogamesapigraphql/VideogamesapiGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.epaitoo.videogamesapigraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamesapiGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamesapiGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epaitoo/videogamesapigraphql/VideogamesapiGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epaitoo.videogamesapigraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamesapiGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
